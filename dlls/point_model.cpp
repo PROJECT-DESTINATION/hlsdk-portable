78e2a92122b2d2ebf5b3d198adb0e17845222b75
relt-1 <60782515+relt-1@users.noreply.github.com>
2024-01-21 16:43:06 +0100

    Upgrade point_model

--- dlls/point_model.cpp
+++ dlls/point_model.cpp
@@ -4,6 +4,34 @@
 #include "animation.h"
 
 
+/*
+============
+COM_FileExtension
+============
+*/
+const char* COM_FileExtension(const char* in)
+{
+	const char* separator, * backslash, * colon, * dot;
+
+	separator = strrchr(in, '/');
+	backslash = strrchr(in, '\\');
+
+	if (!separator || separator < backslash)
+		separator = backslash;
+
+	colon = strrchr(in, ':');
+
+	if (!separator || separator < colon)
+		separator = colon;
+
+	dot = strrchr(in, '.');
+
+	if (dot == NULL || (separator && (dot < separator)))
+		return "";
+
+	return dot + 1;
+}
+
 class CModel : public CBaseEntity
 {
 	void Spawn(void);
@@ -11,6 +39,7 @@ class CModel : public CBaseEntity
 
 void CModel::Spawn()
 {
+	
 	const char* szModel = STRING(pev->model);
 	if (!szModel || !*szModel)
 	{
@@ -18,7 +47,18 @@ void CModel::Spawn()
 		REMOVE_ENTITY(ENT(pev));
 		return;
 	}
+	const char* ext = COM_FileExtension(szModel);
 
+	if (!strcmp(ext, "bsp"))
+	{
+		pev->solid = SOLID_BSP;
+		pev->movetype = MOVETYPE_PUSHSTEP;
+	}
+	else
+	{
+		pev->solid = SOLID_BBOX;
+		pev->movetype = MOVETYPE_TOSS;
+	}
 	pev->classname = MAKE_STRING("point_model");
 	PRECACHE_MODEL(szModel);
 	SET_MODEL(ENT(pev), szModel);
